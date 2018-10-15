a2f3990870d2f6ad3881d7be0733163c62b3f6b5
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-10-15 09:47:18 +0800

    前台模板修改

--- reception/js/custom.js
+++ reception/js/custom.js
@@ -1,177 +1,162 @@
-jQuery(document).ready(function() {
+/******************************************
+    Version: 1.0
+/****************************************** */
+
+(function($) {
     "use strict";
 
-
-/* ------- Preloader ------ */
-jQuery(window).load(function() {
-    jQuery(".status").fadeOut();
-    jQuery(".preloader").delay(1000).fadeOut("slow");
-});
-
-
-/* -------- Appears Menu ------ */
-	$(window).on('ready , scroll', function() {
-	    if ($(window).scrollTop() > 30) {
-	        $('.main-menu').addClass('minified');
-	    } else {
-	        $('.main-menu').removeClass('minified');
-	    }
-	});
-
-/* ---------- Hide Menu-------- */
-  jQuery(".nav a").on("click", function () {
-      jQuery("#nav-menu").removeClass("in").addClass("collapse");
-  });
-
-/* --------- One Page Navigation -------- */
-	$('#collapsingNavbar').onePageNav({
-	    currentClass: 'active',
-	    scrollSpeed: 500,
-	    easing: 'linear'
-	});
-
-/* ---------- Wow Js ---------- */
-var wow = new WOW(
-  {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset:       250,          // distance to the element when triggering the animation (default is 0)
-    mobile:       true,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
-    callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    }
-  }
-);
-wow.init();
-
-/*--------Client----------*/
-//Owl Carousel
-$('#clients-carousel').owlCarousel({
-    items:4,
-    itemsTablet:3,
-    margin:90,
-    stagePadding:90,
-    smartSpeed:450,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,3],
-    itemsTablet: [767,2],
-    itemsTabletSmall: [480,2],
-    itemsMobile : [479,1],
-});
-
-/* --------- Scroll Up --------- */
-	$.scrollUp({
-		scrollName: 'scrollUp', // Element ID
-		scrollDistance: 300, // Distance from top/bottom before showing element (px)
-		scrollFrom: 'top', // 'top' or 'bottom'
-		scrollSpeed: 5000, // Speed back to top (ms)
-		easingType: 'linear', // Scroll to top easing (see http://easings.net/)
-		animation: 'fade', // Fade, slide, none
-		animationInSpeed: 200, // Animation in speed (ms)
-		animationOutSpeed: 200, // Animation out speed (ms)
-		scrollText: 'Scroll to top', // Text for element, can contain HTML
-		scrollTitle: false, // Set a custom <a> title if required. Defaults to scrollText
-		scrollImg: true, // Set true to use image
-		activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-		zIndex: 99998 // Z-Index for the overlay
-	});
-
-/* ---------- lightbox ---------- */
-	$('.featured-work-img').magnificPopup({
-	  type: 'image',
-	  gallery:{
-	    enabled:true
-	  }
-	});
-
-	$('.flickr-gallery-img').magnificPopup({
-	  type: 'image',
-	  gallery:{
-	    enabled:true
-	  }
-	});
-
-
-/* --------- Carousel Slider ---------- */
-
-	// Feature Works
-	$("#teams").owlCarousel({
-		items : 3,
-		itemsDesktop: [1199,1],
-		itemsDesktopSmall: [979,1],
-		itemsTablet: [768,1],
-		itemsMobile : [520,1],
-		autoPlay: 5000
-	});
-
-/* ------------ Stellar ----------- */
-$(window).stellar({
-	responsive: true,
-    positionProperty: 'position'
-});
-
-/* ---------- ISoptope --------- */
-  var $container = $('.portfolio-items');
-
-  // filter items on button click
-   $container.isotope({
-          filter: '*',
-          itemSelector: '.item',
-          animationOptions: {
-              duration: 750,
-              easing: 'linear',
-              queue: false
-          }
-      });
-
-
-  $('#portfolio-filter ul li a').on('click',function(){
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-          filter: selector,
-          animationOptions: {
-              duration: 750,
-              easing: 'linear',
-              queue: false
-          }
-      });
-    return false;
-  });
-
-  var $optionSets = $('#portfolio-filter ul'),
-         $optionLinks = $optionSets.find('a');
-
-         $optionLinks.on('click',function(){
-            var $this = $(this);
-        // don't proceed if already selected
-        if ( $this.hasClass('selected') ) {
-            return false;
-        }
-     var $optionSet = $this.parents('#portfolio-filter ul');
-     $optionSet.find('.selected').removeClass('selected');
-     $this.addClass('selected');
-  });
-
-/* ------------ Home Slider ------------- */
-$( '#slider' ).sliderPro({
-	width: '100%',
-    height: 768,
-    fade: true,
-    waitForLayers: true,
-    buttons: true,
-    autoplay: true,
-    autoScaleLayers: false,
-    slideAnimationDuration: 1500,
-    breakpoints: {
-        600: {
-            height: 480
-        }
+    /* ==============================================
+    Fixed menu
+    =============================================== */
+    
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.header_style_01').addClass('fixed-menu');
+		} else {
+			$('.header_style_01').removeClass('fixed-menu');
+		}
+	});	
+	
+	/* ==============================================
+		Scroll to top  
+	============================================== */
+		
+	if ($('#scroll-to-top').length) {
+		var scrollTrigger = 100, // px
+			backToTop = function () {
+				var scrollTop = $(window).scrollTop();
+				if (scrollTop > scrollTrigger) {
+					$('#scroll-to-top').addClass('show');
+				} else {
+					$('#scroll-to-top').removeClass('show');
+				}
+			};
+		backToTop();
+		$(window).on('scroll', function () {
+			backToTop();
+		});
+		$('#scroll-to-top').on('click', function (e) {
+			e.preventDefault();
+			$('html,body').animate({
+				scrollTop: 0
+			}, 700);
+		});
 	}
-});
+	
+    /* ==============================================
+       LOADER -->
+        =============================================== */
 
+    $(window).load(function() {
+        $("#preloader").on(500).fadeOut();
+        $(".preloader").on(600).fadeOut("slow");
+    });
 
-});
+    /* ==============================================
+     FUN FACTS -->
+     =============================================== */
 
+    function count($this) {
+        var current = parseInt($this.html(), 10);
+        current = current + 50; /* Where 50 is increment */
+        $this.html(++current);
+        if (current > $this.data('count')) {
+            $this.html($this.data('count'));
+        } else {
+            setTimeout(function() {
+                count($this)
+            }, 30);
+        }
+    }
+    $(".stat_count, .stat_count_download").each(function() {
+        $(this).data('count', parseInt($(this).html(), 10));
+        $(this).html('0');
+        count($(this));
+    });
+
+    /* ==============================================
+     TOOLTIP -->
+     =============================================== */
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+
+    /* ==============================================
+     CONTACT -->
+     =============================================== */
+    jQuery(document).ready(function() {
+        $('#contactform').submit(function() {
+            var action = $(this).attr('action');
+            $("#message").slideUp(750, function() {
+                $('#message').hide();
+                $('#submit')
+                    .after('<img src="images/ajax-loader.gif" class="loader" />')
+                    .attr('disabled', 'disabled');
+                $.post(action, {
+                        first_name: $('#first_name').val(),
+                        last_name: $('#last_name').val(),
+                        email: $('#email').val(),
+                        phone: $('#phone').val(),
+                        select_service: $('#select_service').val(),
+                        select_price: $('#select_price').val(),
+                        comments: $('#comments').val(),
+                        verify: $('#verify').val()
+                    },
+                    function(data) {
+                        document.getElementById('message').innerHTML = data;
+                        $('#message').slideDown('slow');
+                        $('#contactform img.loader').fadeOut('slow', function() {
+                            $(this).remove()
+                        });
+                        $('#submit').removeAttr('disabled');
+                        if (data.match('success') != null) $('#contactform').slideUp('slow');
+                    }
+                );
+            });
+            return false;
+        });
+    });
+
+    /* ==============================================
+     CODE WRAPPER -->
+     =============================================== */
+
+    $('.code-wrapper').on("mousemove", function(e) {
+        var offsets = $(this).offset();
+        var fullWidth = $(this).width();
+        var mouseX = e.pageX - offsets.left;
+
+        if (mouseX < 0) {
+            mouseX = 0;
+        } else if (mouseX > fullWidth) {
+            mouseX = fullWidth
+        }
+
+        $(this).parent().find('.divider-bar').css({
+            left: mouseX,
+            transition: 'none'
+        });
+        $(this).find('.design-wrapper').css({
+            transform: 'translateX(' + (mouseX) + 'px)',
+            transition: 'none'
+        });
+        $(this).find('.design-image').css({
+            transform: 'translateX(' + (-1 * mouseX) + 'px)',
+            transition: 'none'
+        });
+    });
+    $('.divider-wrapper').on("mouseleave", function() {
+        $(this).parent().find('.divider-bar').css({
+            left: '50%',
+            transition: 'all .3s'
+        });
+        $(this).find('.design-wrapper').css({
+            transform: 'translateX(50%)',
+            transition: 'all .3s'
+        });
+        $(this).find('.design-image').css({
+            transform: 'translateX(-50%)',
+            transition: 'all .3s'
+        });
+    });
+
+})(jQuery);
