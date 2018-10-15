03eca4f6dd40cf6d775beb84e392dd4f5158e83d
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-10-15 16:27:36 +0800

    no message

--- backstage/controllers/member/LoginController.php
+++ backstage/controllers/member/LoginController.php
@@ -22,6 +22,7 @@ class LoginController extends BasisController
      */
     public function actionLogin()
     {
+        var_dump(111);exit();
         $this->layout = false;
         if (\Yii::$app->request->isPost) {
             $data = \Yii::$app->request->post();
