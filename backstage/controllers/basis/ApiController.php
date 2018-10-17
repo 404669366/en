d48f52d63649f181ce4f67efe87d6e32d430b1b0
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-10-17 15:33:07 +0800

    no message

--- backstage/controllers/basis/ApiController.php
+++ backstage/controllers/basis/ApiController.php
@@ -9,18 +9,10 @@
 namespace app\controllers\basis;
 
 
-use vendor\helpers\Helper;
 use yii\web\Controller;
 
 class ApiController extends Controller
 {
-    public function beforeAction($action)
-    {
-        $re = parent::beforeAction($action);
-        var_dump(Helper::getIp());exit();
-        return $re;
-    }
-
     /**
      * 返回json数据
      * @param array $data
