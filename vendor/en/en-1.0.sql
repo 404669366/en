1d2c2114a5ca2690140296349d01047940bdf7d2
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-10-19 10:24:08 +0800

    sql

--- vendor/en/en-1.0.sql
+++ vendor/en/en-1.0.sql
@@ -1,19 +1,39 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 本地
-Source Server Version : 50553
-Source Host           : localhost:3306
+Source Server         : 服务器A
+Source Server Version : 50641
+Source Host           : 47.99.36.149:3306
 Source Database       : en
 
 Target Server Type    : MYSQL
-Target Server Version : 50553
+Target Server Version : 50641
 File Encoding         : 65001
 
-Date: 2018-10-17 15:46:58
+Date: 2018-10-19 10:23:31
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for en_amend
+-- ----------------------------
+DROP TABLE IF EXISTS `en_amend`;
+CREATE TABLE `en_amend` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT '' COMMENT '用户名',
+  `tel` varchar(11) DEFAULT '' COMMENT '电话号码',
+  `type` tinyint(1) unsigned DEFAULT '1' COMMENT '业务类型 1寻找场地2寻找投资3购买电桩4业务咨询',
+  `status` tinyint(1) unsigned DEFAULT '1' COMMENT '状态 1待跟踪2已联系3已处理4删除',
+  `remark` varchar(255) DEFAULT '' COMMENT '备注',
+  `contact_at` int(11) unsigned DEFAULT '0' COMMENT '联系时间',
+  `created_at` int(11) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='前台用户信息';
+
+-- ----------------------------
+-- Records of en_amend
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for en_job
@@ -25,12 +45,13 @@ CREATE TABLE `en_job` (
   `last` int(11) unsigned DEFAULT '0' COMMENT '上级',
   `powers` varchar(1000) DEFAULT '' COMMENT '拥有权限',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户职位表';
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户职位表';
 
 -- ----------------------------
 -- Records of en_job
 -- ----------------------------
-INSERT INTO `en_job` VALUES ('1', 'admin', '0', '1,2,3,4');
+INSERT INTO `en_job` VALUES ('1', 'root', '0', '1,2,3,4');
+INSERT INTO `en_job` VALUES ('2', 'admin', '1', '1,2,4');
 
 -- ----------------------------
 -- Table structure for en_member
@@ -51,7 +72,7 @@ CREATE TABLE `en_member` (
 -- Records of en_member
 -- ----------------------------
 INSERT INTO `en_member` VALUES ('1', 'root', '18683509267', '$2y$13$jS5psKZ3DusBGXFH.gcUju9y/.TZo6Ahjyga2JJQTYlvLUoUknROG', '1', '1', '1539588593');
-INSERT INTO `en_member` VALUES ('2', 'admin', '18308412675', '$2y$13$l3ZgiX.CZVN/6Hz18lKhZuQlYjqMwui8q0wyqD7gqkrEVmCajOpwC', '1', '1', '1539739444');
+INSERT INTO `en_member` VALUES ('2', 'admin', '18308412675', '$2y$13$3BnTDpK9Jvz6LyiorzDmseXMqig7V/ty5MVGDT1VXykG9pPhDevJe', '2', '1', '1539739444');
 
 -- ----------------------------
 -- Table structure for en_power
