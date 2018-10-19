c5da2e71ac11f2cac7418d7dbbd249a72a95d43b
DESKTOP-DSUIKPF\lt <244652460@qq.com>
2018-10-19 10:26:04 +0800

    前台页面

--- reception/views/menu/menu/index.php
+++ reception/views/menu/menu/index.php
@@ -22,22 +22,23 @@
                     <form id="contactform1" class="row" name="contactform" method="post">
                         <fieldset class="row-fluid">
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" name="name" id="name" class="form-control x" placeholder="姓名" onchange="data()">
+                                <input type="text" name="name"  class="form-control name" placeholder="姓名" >
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" name="tel" id="tel" class="form-control" placeholder="电话" onchange="data()">
+                                <input type="text" name="tel"  class="form-control tel" placeholder="电话" >
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                 <label class="sr-only">Select Time</label>
-                                <select name="business" id="type" class="selectpicker form-control" data-style="btn-white" onchange="data()">
+                                <select name="business"  class="selectpicker form-control type"
+                                        data-style="btn-white">
                                     <option value="0">业务</option>
-                                    <option value="1">Weekdays</option>
-                                    <option value="2">Weekend</option>
-                                    <option value="3">Weekend</option>
+                                    <?php foreach (\vendor\helpers\Constant::amendType() as $k=>$v):?>
+                                        <option value="<?=$k?>"><?=$v?></option>
+                                    <?php endforeach;?>
                                 </select>
                             </div>
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-                                <button type="submit" value="SEND" id="submit1" class="btn btn-light btn-radius btn-brd grd1 btn-block">提交</button>
+                                <button type="button" value="SEND"  class="btn btn-light btn-radius btn-brd grd1 btn-block up">提交</button>
                             </div>
                         </fieldset>
                     </form>
@@ -150,3 +151,42 @@
         </div><!-- end row -->
     </div>
 </div>
+<script type="text/javascript">
+    $('.up').click(function () {
+        var data = {
+            name: $('.name').val(),
+            tel: $('.tel').val(),
+            type: $('.type').val(),
+        }
+        if(checkData(data)){
+            $.getJSON('/amend/amend/save',data,function (re) {
+                if(re.type){
+                    layer.msg('提交成功，我们会尽快联系您')
+                }else{
+                    layer.msg(re.msg);
+                }
+            })
+        }
+        function checkData(data) {
+            if(!data.name){
+                layer.msg('请填写姓名');
+                return false;
+            }
+            if(!data.tel){
+                layer.msg('请填写手机号');
+                return false;
+            }
+            if(!(/^1(3|4|5|7|8)\d{9}$/.test(data.tel))){
+                layer.msg('手机号有误');
+                return false;
+            }
+            if(data.type === '0'){
+                layer.msg('请填写业务类型');
+                return false;
+            }
+            return true;
+        }
+
+    });
+
+</script>
