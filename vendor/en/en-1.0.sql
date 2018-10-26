d947370b2f676f4329cb50837cfeb2c0d2bc2a3b
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-10-26 14:53:09 +0800

    第一阶段完成

--- vendor/en/en-1.0.sql
+++ vendor/en/en-1.0.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50641
 File Encoding         : 65001
 
-Date: 2018-10-25 17:57:06
+Date: 2018-10-26 14:52:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -50,10 +50,9 @@ CREATE TABLE `en_broadcast` (
 -- ----------------------------
 -- Records of en_broadcast
 -- ----------------------------
-INSERT INTO `en_broadcast` VALUES ('1', '轮播1', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181024145526oresnqdrt.jpg', '1');
-INSERT INTO `en_broadcast` VALUES ('2', '轮播2', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181024145128hcclxo3wl.jpg', '2');
-INSERT INTO `en_broadcast` VALUES ('3', '轮播3', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/201810241452511zpicq11z.jpg', '3');
-INSERT INTO `en_broadcast` VALUES ('4', '轮播4', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181024145541yr6hc2vb2.jpg', '4');
+INSERT INTO `en_broadcast` VALUES ('1', '轮播1', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181026112346fh767k51w.jpg', '1');
+INSERT INTO `en_broadcast` VALUES ('2', '轮播2', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181026114105wig44qnk9.jpg', '2');
+INSERT INTO `en_broadcast` VALUES ('3', '轮播3', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181026123007z5ae9ioxp.jpg', '3');
 
 -- ----------------------------
 -- Table structure for en_contact
@@ -88,17 +87,13 @@ CREATE TABLE `en_content` (
 -- ----------------------------
 -- Records of en_content
 -- ----------------------------
-INSERT INTO `en_content` VALUES ('2', 'eQzruthc', '<h1 style=\"text-align: center; \">\n\n\n</h1><h1 style=\"box-sizing: border-box; margin: 5px 0px 10px; font-size: 30px; font-family: \'open sans\', \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 100; line-height: 1.1; font-style: normal; font-variant: normal; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-align: center;\"><font style=\"box-sizing: border-box;\" color=\"#424242\"><br class=\"Apple-interchange-newline\"><br style=\"box-sizing: border-box;\"></font></h1><h1 style=\"text-align: center; \"><font color=\"#424242\"><br class=\"Apple-interchange-newline\"></font></h1><h1 style=\"text-align: center; \"><font color=\"#ffffff\">首页1</font><br></h1>', '首页1', '1', '1540278816');
-INSERT INTO `en_content` VALUES ('3', 'r9yrsiQg', '<h1 style=\"text-align: center; \">首页2</h1>', '首页2', '1', '1540281616');
-INSERT INTO `en_content` VALUES ('4', 'p8endget', '<h1 style=\"text-align: center; \"><font color=\"#ffffff\">首页3</font></h1>', '首页3', '1', '1540281966');
-INSERT INTO `en_content` VALUES ('5', 'a5uofucx', '<h1 style=\"text-align: center; \"><font color=\"#ffffff\">首页4</font></h1>', '首页4', '1', '1540281998');
-INSERT INTO `en_content` VALUES ('6', '8aw3Qa9b', '<h1 style=\"text-align: center; \">公共底部1</h1>', '公共底部1', '1', '1540282023');
-INSERT INTO `en_content` VALUES ('7', 'dzmQzbjp', '<h1 style=\"text-align: center; \"><font color=\"#ffffff\">公共底部2</font></h1>', '公共底部2', '1', '1540282051');
-INSERT INTO `en_content` VALUES ('8', 't7pykzyu', '<h1 style=\"text-align: center; \">产品简介1</h1>', '产品简介1', '1', '1540283054');
-INSERT INTO `en_content` VALUES ('9', 'bkj21hap', '<h1 style=\"text-align: center; \">产品展示1</h1>', '产品展示1', '1', '1540283240');
-INSERT INTO `en_content` VALUES ('10', '6ozqdcjg', '<h1 style=\"text-align: center; \">成功案例1</h1>', '成功案例1', '1', '1540283280');
-INSERT INTO `en_content` VALUES ('11', 'Q1d4n865', '<h1 style=\"text-align: center; \">开放平台1</h1>', '开放平台1', '1', '1540283307');
-INSERT INTO `en_content` VALUES ('12', 'di9lfk7l', '<h1 style=\"text-align: center; \">联系我们1</h1>', '联系我们1', '1', '1540360213');
+INSERT INTO `en_content` VALUES ('2', 'eQzruthc', '<h2 style=\"text-align: center;\"><span style=\"font-size: 22px;\"><font color=\"#ffffff\">﻿新能源汽车充电项目投资信息平台</font></span></h2><font color=\"#ffffff\"><span style=\"font-size: 18px;\"><div style=\"text-align: center;\">充电领域AB端服务提供商</div></span><span style=\"font-size: 18px;\"><div style=\"text-align: center;\">提供全国公共充电桩、站投资需求信息</div></span><span style=\"font-size: 18px;\"><div style=\"text-align: center;\">配套设备供应厂家，项目预算工程申报服务</div></span><span style=\"font-size: 18px;\"><div style=\"text-align: center;\">多家运营商合作，全国代理合伙人招募</div></span></font>', '首页1', '1', '1540278816');
+INSERT INTO `en_content` VALUES ('3', 'r9yrsiQg', '<h1 style=\"text-align: center; \">亿能介绍</h1>', '首页2', '1', '1540281616');
+INSERT INTO `en_content` VALUES ('6', '8aw3Qa9b', '<h1 style=\"text-align: center; \">大区销售经理</h1>', '公共底部1', '1', '1540282023');
+INSERT INTO `en_content` VALUES ('7', 'dzmQzbjp', '<h1 style=\"text-align: center; \"><font color=\"#ffffff\">微信公众号</font></h1>', '公共底部2', '1', '1540282051');
+INSERT INTO `en_content` VALUES ('9', 'bkj21hap', '<h1 style=\"text-align: center; \">开放平台1</h1>', '开放平台1', '1', '1540283240');
+INSERT INTO `en_content` VALUES ('10', '6ozqdcjg', '<h1 style=\"text-align: center; \">合作商案例展示</h1>', '成功案例1', '1', '1540283280');
+INSERT INTO `en_content` VALUES ('12', 'di9lfk7l', '<h1 style=\"text-align: center; \">联系我们</h1><p style=\"text-align: center;\">电话：</p><p style=\"text-align: center;\">邮箱：</p><p style=\"text-align: center;\">地址：成都市高新区天府一街两江国际B座2301</p><div style=\"text-align: center;\"><br></div>', '联系我们1', '1', '1540360213');
 
 -- ----------------------------
 -- Table structure for en_friend
@@ -132,10 +127,10 @@ CREATE TABLE `en_images` (
 -- ----------------------------
 -- Records of en_images
 -- ----------------------------
-INSERT INTO `en_images` VALUES ('1', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181024162840m858cx3st.jpg', '案例1', '1');
-INSERT INTO `en_images` VALUES ('2', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181024163002tcggwrpb5.jpg', '案例2', '2');
-INSERT INTO `en_images` VALUES ('3', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181024163023gg4bg5o98.jpg', '案例3', '3');
-INSERT INTO `en_images` VALUES ('4', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181024163042p7zmkw2ja.jpg', '案例4', '4');
+INSERT INTO `en_images` VALUES ('1', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/201810261451136anho5qk9.jpg', '案例1', '1');
+INSERT INTO `en_images` VALUES ('2', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181026145127dvyjrjqx9.png', '案例2', '2');
+INSERT INTO `en_images` VALUES ('3', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/2018102614514765ew2ur1u.png', '案例3', '3');
+INSERT INTO `en_images` VALUES ('4', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181026145207j42gocicv.jpg', '案例4', '4');
 
 -- ----------------------------
 -- Table structure for en_job
@@ -152,8 +147,8 @@ CREATE TABLE `en_job` (
 -- ----------------------------
 -- Records of en_job
 -- ----------------------------
-INSERT INTO `en_job` VALUES ('1', 'root', '0', '1,2,3,4,5,8,9,10,11,12,13,14,6,7');
-INSERT INTO `en_job` VALUES ('2', 'admin', '1', '1,2,4,5,8,9,10,11,12,13,14,6,7');
+INSERT INTO `en_job` VALUES ('1', 'root', '0', '1,2,3,4,5,8,9,10,11,12,13,14,15,16,6,7');
+INSERT INTO `en_job` VALUES ('2', 'admin', '1', '1,2,4,5,8,9,10,11,12,13,14,15,16,6,7');
 
 -- ----------------------------
 -- Table structure for en_member
@@ -177,24 +172,6 @@ INSERT INTO `en_member` VALUES ('1', 'root', '18683509267', '$2y$13$jS5psKZ3DusB
 INSERT INTO `en_member` VALUES ('2', 'admin', '18308412675', '$2y$13$3BnTDpK9Jvz6LyiorzDmseXMqig7V/ty5MVGDT1VXykG9pPhDevJe', '2', '1', '1539739444');
 
 -- ----------------------------
--- Table structure for en_module
--- ----------------------------
-DROP TABLE IF EXISTS `en_module`;
-CREATE TABLE `en_module` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT '' COMMENT '标题',
-  `image` varchar(1000) DEFAULT '' COMMENT '图片',
-  `content` varchar(1000) DEFAULT '' COMMENT '内容',
-  `remark` varchar(255) DEFAULT '' COMMENT '备注',
-  `sort` tinyint(2) unsigned DEFAULT '0' COMMENT '排序',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='前台模块表';
-
--- ----------------------------
--- Records of en_module
--- ----------------------------
-
--- ----------------------------
 -- Table structure for en_nav
 -- ----------------------------
 DROP TABLE IF EXISTS `en_nav`;
@@ -211,10 +188,27 @@ CREATE TABLE `en_nav` (
 -- Records of en_nav
 -- ----------------------------
 INSERT INTO `en_nav` VALUES ('2', '产品简介', '/menu/menu/about', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181025175356b9gyfzajz.jpg', '2');
-INSERT INTO `en_nav` VALUES ('3', '产品展示', '/menu/menu/service', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181025175409mn1h7oclz.jpg', '3');
+INSERT INTO `en_nav` VALUES ('3', '开放平台', '/menu/menu/service', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181025175409mn1h7oclz.jpg', '5');
 INSERT INTO `en_nav` VALUES ('4', '成功案例', '/menu/menu/gallery', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/20181025175422xihmvaanl.jpg', '4');
-INSERT INTO `en_nav` VALUES ('5', '开放平台', '/menu/menu/properties', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/201810251754426lozxcjz5.jpg', '5');
 INSERT INTO `en_nav` VALUES ('6', '联系我们', '/menu/menu/contact', 'https://ascasc.oss-cn-hangzhou.aliyuncs.com/201810251754548omQfuiQp.jpg', '6');
+
+-- ----------------------------
+-- Table structure for en_open
+-- ----------------------------
+DROP TABLE IF EXISTS `en_open`;
+CREATE TABLE `en_open` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT '' COMMENT '标题',
+  `image` varchar(500) DEFAULT '' COMMENT '图片',
+  `content` varchar(3000) DEFAULT '' COMMENT '文本内容',
+  `link` varchar(300) DEFAULT '' COMMENT '链接',
+  `sort` tinyint(2) unsigned DEFAULT '0' COMMENT '排序',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='开放平台配置表';
+
+-- ----------------------------
+-- Records of en_open
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for en_power
@@ -229,7 +223,7 @@ CREATE TABLE `en_power` (
   `url` varchar(100) DEFAULT '' COMMENT '权限路由',
   `sort` int(10) unsigned DEFAULT '0' COMMENT '排序',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='用户权限表';
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='用户权限表';
 
 -- ----------------------------
 -- Records of en_power
@@ -245,6 +239,46 @@ INSERT INTO `en_power` VALUES ('8', '5', '4e57tirz', '1', '内容列表', '/cont
 INSERT INTO `en_power` VALUES ('9', '5', 'glpm5l3Q', '1', '导航管理', '/content/nav/list', '999');
 INSERT INTO `en_power` VALUES ('10', '5', 'r8ojb371', '1', '轮播管理', '/content/bro/list', '998');
 INSERT INTO `en_power` VALUES ('11', '5', '2jwoicom', '1', '案例管理', '/content/image/list', '997');
-INSERT INTO `en_power` VALUES ('12', '5', 'lkbtlnfc', '1', '模块管理', '/content/module/list', '996');
+INSERT INTO `en_power` VALUES ('12', '5', 'lkbtlnfc', '1', '服务管理', '/content/serve/list', '996');
 INSERT INTO `en_power` VALUES ('13', '5', 'yi17v4vi', '1', '友情链接管理', '/content/friend/list', '994');
 INSERT INTO `en_power` VALUES ('14', '5', 'no1vvr5l', '1', '联系方式管理', '/content/contact/list', '993');
+INSERT INTO `en_power` VALUES ('15', '5', 'z4g2fhea', '1', '开放管理', '/content/open/list', '995');
+INSERT INTO `en_power` VALUES ('16', '5', 'yfsaclxf', '1', '业务员信息管理', '/content/salesman/list', '992');
+
+-- ----------------------------
+-- Table structure for en_salesman
+-- ----------------------------
+DROP TABLE IF EXISTS `en_salesman`;
+CREATE TABLE `en_salesman` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `region` varchar(255) DEFAULT '' COMMENT '所在地理位置',
+  `intro` varchar(2000) DEFAULT '' COMMENT '介绍',
+  `image` varchar(1000) DEFAULT '' COMMENT '头像',
+  `name` varchar(10) DEFAULT '' COMMENT '姓名',
+  `tel` varchar(11) DEFAULT '' COMMENT '电话',
+  `sort` tinyint(2) DEFAULT '0' COMMENT '排序',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='业务员配置表';
+
+-- ----------------------------
+-- Records of en_salesman
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for en_serve
+-- ----------------------------
+DROP TABLE IF EXISTS `en_serve`;
+CREATE TABLE `en_serve` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT '' COMMENT '服务名称',
+  `smallImage` varchar(500) DEFAULT '' COMMENT '服务小图',
+  `bigImage` varchar(500) DEFAULT '' COMMENT '服务大图',
+  `resume` varchar(255) DEFAULT '' COMMENT '服务简述',
+  `content` varchar(5000) DEFAULT '' COMMENT '服务详情',
+  `sort` int(10) unsigned DEFAULT '0' COMMENT '排序',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='服务配置表';
+
+-- ----------------------------
+-- Records of en_serve
+-- ----------------------------
