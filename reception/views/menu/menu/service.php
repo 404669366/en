769bd6fe889cca0f9980ae3449f0b631ce66e692
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-10-26 09:07:24 +0800

    no message

--- reception/views/menu/menu/service.php
+++ reception/views/menu/menu/service.php
@@ -5,40 +5,19 @@
                 <?= \vendor\en\EnContentBase::getContent("bkj21hap") ?>
             </div>
         </div>
+        <style>
+            .marginBottom{
+                margin-bottom: 3rem;
+            }
+        </style>
         <div class="row">
-            <div class="col-md-4 col-sm-4 col-sm-12">
+            <div class="col-md-4 col-sm-4 col-sm-12 marginBottom">
                 <div class="single-services">
                     <div class="single-services-img">
                         <img src="/resource/uploads/icon-01.png" class="img-responsive" alt="">
                     </div>
                     <div class="single-services-desc">
-                        <h4><a href="#">Full Furnished</a></h4>
-                        <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam
-                            risus. Sed a tellus quis mi rhoncus dignissim.</p>
-                    </div>
-                    <a href="#" class="btn btn-light btn-radius grd1 btn-brd"> Read More </a>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4 col-sm-12">
-                <div class="single-services">
-                    <div class="single-services-img">
-                        <img src="/resource/uploads/icon-02.png" class="img-responsive" alt="">
-                    </div>
-                    <div class="single-services-desc">
-                        <h4><a href="#">Living Inside a Nature</a></h4>
-                        <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam
-                            risus. Sed a tellus quis mi rhoncus dignissim.</p>
-                    </div>
-                    <a href="#" class="btn btn-light btn-radius grd1 btn-brd"> Read More </a>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4 col-sm-12">
-                <div class="single-services">
-                    <div class="single-services-img">
-                        <img src="/resource/uploads/icon-03.png" class="img-responsive" alt="">
-                    </div>
-                    <div class="single-services-desc">
-                        <h4><a href="#">Luxurious Fittings</a></h4>
+                        <h4>Full Furnished</h4>
                         <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam
                             risus. Sed a tellus quis mi rhoncus dignissim.</p>
                     </div>
