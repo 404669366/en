e9edf50da1a98ef402aac56bb62ee6ccb6c13f33
DESKTOP-DSUIKPF\lt <244652460@qq.com>
2018-11-07 11:18:35 +0800

    no message

--- reception/views/product/product/detail.php
+++ reception/views/product/product/detail.php
@@ -1,5 +1,5 @@
 <div class="about-box">
-    <img src="<?= $model['image'] ?>" style="margin: 0 auto;display: block;height: 30rem;">
+    <img src="<?= $model['image'] ?>" style="margin: 0 auto 4rem auto;display: block;height: 30rem;">
     <div class="row">
         <div class="col-sm-offset-4 col-sm-4 col-xs-offset-1 col-xs-10" style="margin-bottom: 1rem">
             <?= $model['intro'] ?>
