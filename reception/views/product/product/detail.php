ff092641e9aceeab5fccb93b2b8e93d52946c169
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-11-14 10:53:21 +0800

    no message

--- reception/views/product/product/detail.php
+++ reception/views/product/product/detail.php
@@ -1,9 +1,9 @@
 <div class="about-box">
     <div class="row" style="margin:0;">
-        <img class="col-sm-offset-3 col-sm-6 col-xs-offset-2 col-xs-8" src="<?= $model['image'] ?>">
-        <div class="col-sm-offset-3 col-sm-6 col-xs-offset-2 col-xs-8" style="margin-bottom: 1rem">
+        <img class="col-sm-offset-2 col-sm-8 col-xs-offset-2 col-xs-8" src="<?= $model['image'] ?>" style="margin-bottom: 1rem">
+        <div class="col-sm-offset-1 col-sm-10 col-xs-offset-2 col-xs-8" style="margin-bottom: 1rem">
             <?= $model['intro'] ?>
         </div>
-        <img src="<?= $model['parameter']?>" class="col-sm-offset-3 col-sm-6 col-xs-offset-2 col-xs-8">
+        <img src="<?= $model['parameter']?>" class="col-sm-offset-1 col-sm-10 col-xs-offset-2 col-xs-8">
     </div>
 </div>
