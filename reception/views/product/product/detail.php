cb825d64e1df580f9c955fd728736aec73824ef4
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-11-14 10:45:20 +0800

    no message

--- reception/views/product/product/detail.php
+++ reception/views/product/product/detail.php
@@ -1,6 +1,6 @@
 <div class="about-box">
-    <img src="<?= $model['image'] ?>" style="margin: 0 auto 4rem auto;display: block;height: 20rem;">
     <div class="row">
+        <img class="col-sm-offset-4 col-sm-4 col-xs-offset-1 col-xs-10" src="<?= $model['image'] ?>">
         <div class="col-sm-offset-4 col-sm-4 col-xs-offset-1 col-xs-10" style="margin-bottom: 1rem">
             <?= $model['intro'] ?>
         </div>
