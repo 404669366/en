23a7281acf81fd3d786f488a2c0bfc88dd47b597
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-11-14 10:37:29 +0800

    no message

--- reception/views/news/news/detail.php
+++ reception/views/news/news/detail.php
@@ -1,7 +1,7 @@
 <div class="about-box">
     <h1 style="text-align: center; font-size: 38px"><strong><?= $model->title ?></strong></h1>
     <div style="text-align: center;margin-bottom: 4rem"><strong>作者：<?= $model->author ?>&emsp;&emsp;&emsp;&emsp;&emsp;来源：<?= $model->source ?></strong></div>
-    <img src="<?= $model->image ?>" style="margin: 0 auto;display: block;height: 30rem;">
+    <img src="<?= $model->image ?>" style="margin: 0 auto;display: block;height: 20rem;">
     <div style="width: 60%;margin: 5rem auto 0 auto">
         <?= $model->content ?>
     </div>
