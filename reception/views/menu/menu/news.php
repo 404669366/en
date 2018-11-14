0cac2fc2e3b4ac818454597beb5fdebf6489e498
DESKTOP-DSUIKPF\lt <244652460@qq.com>
2018-11-14 10:20:39 +0800

    no message

--- reception/views/menu/menu/news.php
+++ reception/views/menu/menu/news.php
@@ -11,12 +11,11 @@
         length:6,
         callBack: function (row) {
             $('.news').append('<div class="col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 3rem;">\n' +
+                '                <a href="/news/news/detail?id='+row.id+'">' +
                 '                <div class="service-widget">\n' +
                 '                    <div class="property-main">\n' +
                 '                        <div class="property-wrap">\n' +
                 '                            <figure class="post-media wow fadeIn">\n' +
-                '                                <a href="/news/news/detail?id='+row.id+'" class="hoverbutton global-radius"><i\n' +
-                '                                            class="flaticon-unlink"></i></a>\n' +
                 '                                <img src="'+row.image+'" alt="" class="img-responsive" style="height: 30rem">\n' +
                 '                            </figure>\n' +
                 '                            <a href="/news/news/detail?id='+row.id+'">\n' +
@@ -30,6 +29,7 @@
                 '                        </div>\n' +
                 '                    </div>\n' +
                 '                </div>\n' +
+                '                </a>\n' +
                 '            </div>');
         }
     })
