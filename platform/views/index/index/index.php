ca18ec44f481a58a30680bce76a7bdd2937c4887
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2018-11-20 13:42:48 +0800

    no message

---
+++ platform/views/index/index/index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>index</title>
+		<!--引入重置样式-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/reset.css"/>
+		<!--引入公共样式-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/common.css"/>
+		<!--引入index样式-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/index.css"/>
+		<!--引入字体-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/font-awesome.min.css"/>
+	</head>
+	<body>
+		<!--头部start-->
+		<div class="header">
+			<!--nav-->
+			<div class="navTop">
+				<div class="navlogo">
+					<img src="/resources/images/logo.png" />
+				</div>
+				<div class="login float_right">
+					<i class="fa fa-user-circle-o" aria-hidden="true" ></i>
+					<a href="#">登录</a>
+					/
+					<a href="#">注册</a>
+				</div>
+				<ul class="navList">
+					<li><a href="#">首页</a></li>
+					<li><a href="#">业务介绍</a></li>
+					<li><a href="#">成功案例</a></li>
+					<li><a href="#">新闻动态</a></li>
+					<li><a href="#">开放平台</a></li>
+					<li><a href="#">收益预测</a></li>
+					<li><a href="#">联系我们</a></li>
+				</ul>
+			</div>
+			<!--标题+搜索-->
+			<div class="box1200">
+				<!--title-->
+				<div class="head_Tit">
+					<div class="title_small">
+						<span>标题一</span>
+						&nbsp;/&nbsp;
+						<span>标题二</span>
+						&nbsp;/&nbsp;
+						<span>标题三</span>
+					</div>
+					<div class="title_big">海量建桩源 省心上亿能</div>
+				</div>
+				<!--search-->
+				<div class="search">
+					<input type="text" class="search_txt float_left" placeholder="请输入"/>
+					<button type="button" class="search_btn float_left"/>
+						<i class="fa fa-search" aria-hidden="true"></i>
+					</button>
+					
+				</div>
+			</div>
+			<!--清除浮动-->
+			<div class="clear"></div>
+		</div>
+		<!--头部end-->
+		
+		<!--主体内容start-->
+		<div class="content">
+			<!--内容1-->
+			<div class="main1 box1200 marginTop80">
+				<div class="big_tit">
+					标题一
+				</div>
+				<p class="p1">好房源那么多，我们为你精选 <span class="float_right span1"><a href="#">更多推荐</a></span></p>
+				<ul class="main1_ul">
+					<li>
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="main_tit">
+							<a href="#" class="ma1">光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街</a>
+						</p>
+						<p class="small_tit"><a href="#" class="sm_a">光华逸家·3室1厅·93.89平米</a>
+							<span class="price float_right"><a href="#" class="pra">216万</a></span>
+						</p>
+					</li>
+					<li>
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="main_tit">
+							<a href="#" class="ma1">光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街</a>
+						</p>
+						<p class="small_tit"><a href="#" class="sm_a">光华逸家·3室1厅·93.89平米</a>
+							<span class="price float_right"><a href="#" class="pra">216万</a></span>
+						</p>
+					</li>
+					<li>
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="main_tit">
+							<a href="#" class="ma1">光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街</a>
+						</p>
+						<p class="small_tit"><a href="#" class="sm_a">光华逸家·3室1厅·93.89平米</a>
+							<span class="price float_right"><a href="#" class="pra">216万</a></span>
+						</p>
+					</li>
+					<li class="marginLt">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="main_tit">
+							<a href="#" class="ma1">光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街</a>
+						</p>
+						<p class="small_tit"><a href="#" class="sm_a">光华逸家·3室1厅·93.89平米</a>
+							<span class="price float_right"><a href="#" class="pra">216万</a></span>
+						</p>
+					</li>
+				</ul>
+				<!--清除浮动-->
+				<div class="clear"></div>
+			</div>
+			
+			<!--内容2-->
+			<div class="main1 box1200 marginTop80">
+				<div class="big_tit">
+					标题二
+				</div>
+				<p class="p1">纵览小区，恋上这座城<span class="float_right span1"><a href="#">更多成都小区</a></span></p>
+				<ul class="main1_ul2">
+					<li>
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<div class="resblock-desc">
+	                        <span class="ul2_name float_left">南湖国际社区</span>
+	                        <span class="ul2_price float_right"><label>13851</label>元/平</span>
+                    	</div>
+                    	<p class="ul2_smtxt float_left">2010年建</p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<div class="resblock-desc">
+	                        <span class="ul2_name float_left">南湖国际社区</span>
+	                        <span class="ul2_price float_right"><label>13851</label>元/平</span>
+                    	</div>
+                    	<p class="ul2_smtxt float_left">2010年建</p>
+						</a>
+					</li>
+					<li class="marginLt">
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<div class="resblock-desc">
+	                        <span class="ul2_name float_left">南湖国际社区</span>
+	                        <span class="ul2_price float_right"><label>13851</label>元/平</span>
+                    	</div>
+                    	<p class="ul2_smtxt float_left">2010年建</p>
+						</a>
+					</li>
+				</ul>
+				<!--清除浮动-->
+				<div class="clear"></div>
+			</div>
+			
+			<!--内容3-->
+			<div class="main1 box1200 marginTop80">
+				<div class="big_tit">
+					标题三
+				</div>
+				<p class="p1">真实信息准确同步，建桩动态一手掌握<span class="float_right span1"><a href="#">更多成都建桩</a></span></p>
+				<ul class="main1_ul3">
+					<li>
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						</a>
+					</li>
+					<li class="marginLt">
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						</a>
+					</li>
+				</ul>
+				<!--清除浮动-->
+				<div class="clear"></div>
+			</div>
+			
+			<!--内容4-->
+			<div class="main1 box1200 marginTop80">
+				<div class="big_tit">
+					标题四
+				</div>
+				<p class="p1">高品质租房体验，从贝壳开始<span class="float_right span1"><a href="#">更多成都小区</a></span></p>
+				<ul class="main1_ul4">
+					<li>
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="ul4_tit">独栋·乐乎城市青年社区 天府广场店 C户型</p>
+						<p class="ul4_smtit">人民公园 / 1室1厅1卫
+							<span class="ul4_price float_right">2510 元/月</span>
+                        </p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="ul4_tit">独栋·乐乎城市青年社区 天府广场店 C户型</p>
+						<p class="ul4_smtit">人民公园 / 1室1厅1卫
+							<span class="ul4_price float_right">2510 元/月</span>
+                        </p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="ul4_tit">独栋·乐乎城市青年社区 天府广场店 C户型</p>
+						<p class="ul4_smtit">人民公园 / 1室1厅1卫
+							<span class="ul4_price float_right">2510 元/月</span>
+                        </p>
+						</a>
+					</li>
+					<li class="marginLt">
+						<a href="#">
+						<img src="/resources/images/01.jpg" alt="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街" title="光华逸家 精装修 套三双卫 带阳台 朝南对中庭 不临街"/>
+						<p class="ul4_tit">独栋·乐乎城市青年社区 天府广场店 C户型</p>
+						<p class="ul4_smtit">人民公园 / 1室1厅1卫
+							<span class="ul4_price float_right">2510 元/月</span>
+                        </p>
+						</a>
+					</li>
+				</ul>
+				<!--清除浮动-->
+				<div class="clear"></div>
+			</div>
+		</div>
+		<!--主体内容end-->
+		
+		<!--脚部start-->
+		<div class="footer marginTop80">
+			<div class="box1200">
+				<!--合作商-->
+			    <ul class="footer_nav">
+			    	<li><a rel="nofollow" target="_blank" href="//online.unionpay.com/"><img src="/resources/images/logo.png" /></a></li>
+			    	<li><a rel="nofollow" target="_blank" href="//online.unionpay.com/"><img src="/resources/images/logo.png" /></a></li>
+			    	<li><a rel="nofollow" target="_blank" href="//online.unionpay.com/"><img src="/resources/images/logo.png" /></a></li>
+			    	<li><a rel="nofollow" target="_blank" href="//online.unionpay.com/"><img src="/resources/images/logo.png" /></a></li>
+			    	<li><a rel="nofollow" target="_blank" href="//online.unionpay.com/"><img src="/resources/images/logo.png" /></a></li>
+			    	<li><a rel="nofollow" target="_blank" href="//online.unionpay.com/"><img src="/resources/images/logo.png" /></a></li>
+			    	<li><a rel="nofollow" target="_blank" href="//online.unionpay.com/"><img src="/resources/images/logo.png" /></a></li>
+			    </ul>
+			    <!--关于我们-->
+		    	<ul class="footer_list">
+			    	<li><a href="#">关于我们</a>&nbsp; |&nbsp;</li>
+			    	<li><a href="#">关于我们</a>&nbsp; |&nbsp;</li>
+			    	<li><a href="#">关于我们</a>&nbsp; |&nbsp;</li>
+			    	<li><a href="#">关于我们</a>&nbsp; |&nbsp;</li>
+			    	<li><a href="#">关于我们</a>&nbsp; |&nbsp;</li>
+			    	<li><a href="#">关于我们</a>&nbsp; |&nbsp;</li>
+			    	<li><a href="#">关于我们</a>&nbsp; |&nbsp;</li>
+			    	<li><a href="#">关于我们</a></li>
+				</ul>
+				<!--公众号-->
+				<ul class="public">
+					<li><img src="/resources/images/en2.png" /></li>
+					<li><img src="/resources/images/en2.png" /></li>
+				</ul>
+			    <!--底部版权-->
+			    <p>Copyright © 2018 en.link，All Rights Reserved. 四川亿能天成科技有限公司</p>
+			</div>
+		</div>
+		<!--脚部end-->
+	</body>
+</html>
---
+++ platform/views/index/index/list.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<!--引入重置样式-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/reset.css"/>
+		<!--引入公共样式-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/common.css"/>
+		<!--引入index样式-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/list.css"/>
+		<!--引入字体-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/font-awesome.min.css"/>
+	</head>
+	<body>
+		<div class="banner">
+			
+		</div>
+	</body>
+</html>
