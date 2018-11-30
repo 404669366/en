2657baf34a67cd34d424a6158dcf9f2233483778
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2018-11-30 09:50:48 +0800

    no message

---
+++ platform/views/index/index/404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>404</title>
+		 <!--引入重置样式-->
+		<link rel="stylesheet" type="text/css" href="/resources/css/reset.css"/>	
+	</head>
+	<body>
+	</body>
+</html>
