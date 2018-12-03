8d576d4ce11447a4c144d89ab388dc51c0226f2d
DESKTOP-56SB0F0\miku <404669366@qq.com>
2018-12-04 01:44:44 +0800

    no message

--- platform/views/user/user/ident.php
+++ platform/views/user/user/ident.php
@@ -50,8 +50,8 @@
             <?php if ($basisData['isCobber']): ?>
                 <li><a href="/user/user/track-field.html">场地跟踪</a></li>
             <?php endif; ?>
-            <li class="actives"><a href="/user/user/ident.html">认证合伙人</a></li>
             <li><a href="/user/user/update.html">修改密码</a></li>
+            <li class="actives"><a href="/user/user/ident.html">认证合伙人</a></li>
         </ul>
     </div>
 
--- platform/views/user/user/update.php
+++ platform/views/user/user/update.php
@@ -49,10 +49,10 @@
             <?php if ($basisData['isCobber']): ?>
                 <li><a href="/user/user/track-field.html">场地跟踪</a></li>
             <?php endif; ?>
+            <li class="actives"><a href="/user/user/update.html">修改密码</a></li>
             <?php if (!$basisData['isCobber'] || $basisData['isCobber']->type == 1): ?>
                 <li><a href="/user/user/ident.html">认证合伙人</a></li>
             <?php endif; ?>
-            <li class="actives"><a href="/user/user/update.html">修改密码</a></li>
         </ul>
     </div>
 
