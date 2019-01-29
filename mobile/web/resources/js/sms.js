350a15c546d84ea2830a124870e608cb6fd37483
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-01-29 10:24:21 +0800

    no message

--- mobile/web/resources/js/sms.js
+++ mobile/web/resources/js/sms.js
@@ -3,7 +3,7 @@ function sms(config) {
         if (check($(config.telModel).val())) {
             $.getJSON('/sms/sms/send.html', {tel: $(config.telModel).val()}, function (re) {
                 if (re.type) {
-                    layer.msg('验证码发送成功,请注意查收');
+                    layer.msg('<span style="font-size:2.8rem;height:100%;line-height:100%">验证码发送成功,请注意查收</span>');
                     var text = $(config.click).text();
                     var time = config.timeout || 60;
                     $(config.click).text(time + 's').attr('send', true);
@@ -16,7 +16,7 @@ function sms(config) {
                         }
                         $(config.click).text(time + 's');
                     }, 1000);
-                }else {
+                } else {
                     layer.msg(re.msg);
                 }
             })
@@ -25,15 +25,15 @@ function sms(config) {
 
     function check(tel) {
         if (!tel) {
-            layer.msg('请填写手机号');
+            layer.msg('<span style="font-size:2.8rem;height:100%;line-height:100%">请填写手机号</span>');
             return false;
         }
         if (!(/^1(3|4|5|7|8)\d{9}$/.test(tel))) {
-            layer.msg('手机号有误');
+            layer.msg('<span style="font-size:2.8rem;height:100%;line-height:100%">手机号有误</span>');
             return false;
         }
         if ($(config.click).attr('send')) {
-            layer.msg('验证码已发送,请稍后再试');
+            layer.msg('<span style="font-size:2.8rem;height:100%;line-height:100%">验证码已发送,请稍后再试</span>');
             return false;
         }
         return true;
