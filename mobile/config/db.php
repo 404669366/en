7227a1b0c7bc1e978c6ce0fa97f81924dafebce6
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-01-29 13:10:02 +0800

    update .gitignore

--- mobile/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=transaction',
-    'username' => 'root',
-    'password' => 'fi9^BRLHschX%V96',
-    'charset' => 'utf8',
-];
