48b0d11d8f4ee939ca7860f8c4ee343c47a43120
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-03-08 16:53:21 +0800

    no message

--- mobile/views/user/intention/detail.php
+++ mobile/views/user/intention/detail.php
@@ -12,8 +12,6 @@
     <script src="/resources/js/area.js" type="text/javascript" charset="utf-8"></script>
     <script src="/upload/h5-upload.js" type="text/javascript" charset="utf-8"></script>
     <script src="/sinlar/sinlar.js" type="text/javascript" charset="utf-8"></script>
-
-
     <?php \vendor\helpers\Msg::run() ?>
 </head>
 <body>
